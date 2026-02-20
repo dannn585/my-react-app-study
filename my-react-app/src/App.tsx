0b29733ec8fc093db26fc38c2a4d197fe68e7b8b
ダン <ssrdn11@gmail.com>
2026-02-20 16:13:32 +0900

    Tailwindの利用

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>My First Deployed App!</h1>
+      <h1 className = "text-red-500 font-bold">My First Deployed App!</h1>
       <UserList />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
